d0ceae1e3a8557aa7b2a48b89b81131f7ad849fe
Carly Ho <carlymho@fastmail.com>
2023-06-29 20:04:33 -0500

    add config setting for image bg

--- config.php
+++ config.php
@@ -10,6 +10,11 @@ define("DB_LOCATION", "./kudos.db");
 // Something to show in page titles. You could change this to "Kudos for [My Name]", etc.
 define("SITE_NAME", "Kudos");
 
+// This changes the image bgcolor! you probably want it to be something that the text will show up on.
+// use "none" for transparent, but this might cause it to not show up if the background is the same color
+// as the text.
+define("IMAGE_BG", "white");
+
 // Text Drawing Settings
 $id = new ImagickDraw();
 $id->setFont("./fonts/open-sans.ttf");
